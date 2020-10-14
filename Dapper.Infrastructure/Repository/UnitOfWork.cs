75f0dfb524e6972e326161babbcac3153b392c84
min8grace <56316476+min8grace@users.noreply.github.com>
2020-10-14 14:09:38 -0700

    Add project files.

---
+++ Dapper.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(IProductRepository productRepository)
+        {
+            Products = productRepository;
+        }
+        public IProductRepository Products { get; }
+    }
+}
